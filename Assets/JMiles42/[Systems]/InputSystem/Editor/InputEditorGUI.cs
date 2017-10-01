9dd338c487db755a6052544565c4cd55244b03e3
JMiles42 <J@jmiles42.com>
2017-10-01 23:55:33 +1100

    Added Gameplay input manager

--- Assets/JMiles42/[Systems]/InputSystem/Editor/InputEditorGUI.cs
+++ Assets/JMiles42/[Systems]/InputSystem/Editor/InputEditorGUI.cs
@@ -1,31 +1,56 @@
 using JMiles42.Systems.InputManager;
+using JMiles42.Utilities;
 using UnityEditor;
 using UnityEngine;
 
 namespace JMiles42.Editor.PropertyDrawers
 {
-	[CustomPropertyDrawer(typeof (InputAxis))]
-	public class InputEditorGUI: JMilesPropertyDrawer
-	{
-		public override float GetPropertyHeight(SerializedProperty property, GUIContent label) { return EditorGUIUtility.singleLineHeight * 4; }
+    [CustomPropertyDrawer(typeof (InputAxis))]
+    public class InputEditorGUI: JMilesPropertyDrawer
+    {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label) { return EditorGUIUtility.singleLineHeight * 3; }
 
-		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-		{
-			var ValueInverted = new EditorEntry("Invert Result", property.FindPropertyRelative("ValueInverted"));
-			var Axis = new EditorEntry(string.Format("{0} Axis", property.FindPropertyRelative("Axis").stringValue), property.FindPropertyRelative("Axis"));
-			var UAxis = new EditorEntry(string.Format("{0} Unity Axis", property.FindPropertyRelative("UnityAxis").stringValue),
-										property.FindPropertyRelative("UnityAxis"));
-			var m_Value = new EditorEntry(string.Format("{0}Value", (ValueInverted.Property.boolValue? "Non Inverted " : "")), property.FindPropertyRelative("m_Value"));
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (!string.IsNullOrEmpty(label.text))
+            {
+                position.height = singleLine;
+                EditorGUI.LabelField(position, label);
+                position.y += singleLine;
+            }
 
-			var AxisRect = position;
-			AxisRect.height = singleLine;
-			Axis.Draw(AxisRect, EditorEntry.SplitType.Normal);
-			AxisRect.y += singleLine;
-			UAxis.Draw(AxisRect, EditorEntry.SplitType.Normal);
-			AxisRect.y += singleLine;
-			m_Value.Draw(AxisRect, EditorEntry.SplitType.Normal);
-			AxisRect.y += singleLine;
-			ValueInverted.Draw(AxisRect, EditorEntry.SplitType.Normal);
-		}
-	}
+            var ValueInverted = new EditorEntry("Invert Result", property.FindPropertyRelative("ValueInverted"));
+            var Axis = new EditorEntry(string.Format("{0} Axis", property.FindPropertyRelative("Axis").stringValue), property.FindPropertyRelative("Axis"));
+            var UAxis = new EditorEntry(string.Format("{0} Unity Axis", property.FindPropertyRelative("UnityAxis").stringValue),
+                                        property.FindPropertyRelative("UnityAxis"));
+            var m_Value = new EditorEntry(string.Format("{0}Value", (ValueInverted.Property.boolValue? "Non Inverted " : "")),
+                                          property.FindPropertyRelative("m_Value"));
+
+            var AxisRect = position;
+            AxisRect.width = Mathf.Abs(AxisRect.width);
+            AxisRect.height = singleLine;
+
+            var labelWidth = EditorGUIUtility.labelWidth;
+            var fieldWidth = EditorGUIUtility.fieldWidth;
+
+            using (new ActionOnDispose(() => EditorGUIUtility.labelWidth = labelWidth))
+            {
+                using (new ActionOnDispose(() => EditorGUIUtility.fieldWidth = fieldWidth))
+                {
+                    EditorGUIUtility.labelWidth = position.width / 4;
+                    EditorGUIUtility.fieldWidth = position.width / 4;
+                    const EditorEntry.SplitType SPLIT_TYPE = EditorEntry.SplitType.Normal;
+                    {
+                        Axis.Draw(EditorDrawersUtilities.GetRectWidthIndexed(AxisRect, 2, 0), SPLIT_TYPE);
+                        UAxis.Draw(EditorDrawersUtilities.GetRectWidthIndexed(AxisRect, 2, 1, false, false), SPLIT_TYPE);
+                    }
+                    AxisRect.y += singleLine;
+                    {
+                        m_Value.Draw(EditorDrawersUtilities.GetRectWidthIndexed(AxisRect, 2, 0), SPLIT_TYPE);
+                        ValueInverted.Draw(EditorDrawersUtilities.GetRectWidthIndexed(AxisRect, 2, 1, false, false), SPLIT_TYPE);
+                    }
+                }
+            }
+        }
+    }
 }
---
+++ Assets/Scripts/Jordan/Input/GameplayInputManager.cs
@@ -0,0 +1,19 @@
+﻿using JMiles42.Generics;
+using JMiles42.Systems.InputManager;
+using JMiles42.UnityInterfaces;
+
+public class GameplayInputManager: Singleton<GameplayInputManager>, IEventListening
+{
+    public InputAxis PrimaryClick = "Fire1";
+    public InputAxis SecondaryClick = "Fire2";
+
+    public void OnEnable()
+    {
+        
+    }
+
+    public void OnDisable()
+    {
+        
+    }
+}
