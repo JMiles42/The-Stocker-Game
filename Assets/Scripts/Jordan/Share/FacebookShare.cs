0435f3dc5c6e41790577edfc2235722fa73397f5
Unknown <J@jmiles42.com>
2017-10-02 17:08:25 +1100

    Added Facebook & Twitter Sharing

---
+++ Assets/Scripts/Jordan/Share/FacebookShare.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacebookShare
+{
+	public static string FACEBOOK_URL = "http://twitter/intent/tweet";
+	public static string FACEBOOK_LANG = "en";
+	public static string FACEBOOK_SHARE_TEXT = "This is a test ignore me! YAAAAY: {0}";
+
+
+
+}
---
+++ Assets/Scripts/Jordan/Share/TwitterShare.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class TwitterShare {
+	public static string TWITTER_URL = "http://twitter.com/intent/tweet";
+	public static string TWITTER_LANG = "en";
+	public static string TWITTER_SHARE_TEXT = "This is a test ignore me! YAAAAY: {0} YAAY for API learning";
+
+	public static void TweetTweet(string score) {
+		Application.OpenURL(TWITTER_URL + "?text=" + WWW.EscapeURL(string.Format(TWITTER_SHARE_TEXT, score)));
+	}
+
+	public static void TweetTweet(string str, object score) {
+		Application.OpenURL(TWITTER_URL + "?text=" + WWW.EscapeURL(string.Format(str, score)));
+	}
+}
