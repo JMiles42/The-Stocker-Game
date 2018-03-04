5951c7d24b1b7dd11fb4e2fb4d1047c6ad070f1a
JMiles42 <J@jmiles42.com>
2018-03-04 17:32:20 +1100

    The Stocker 1.0t
    Added Closest WorldObject Distance
    This will hopfully be the final State Addition I have to make. as this resets all the learning the MLA has made.

---
+++ Assets/Scripts/Jordan/LinkableLists/WorldObjectPathManager.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ForestOfChaosLib;
+using ForestOfChaosLib.Grid;
+
+[StockerFolder]
+public class WorldObjectPathManager: FoCsScriptableObject
+{
+	private StateClass _state;
+	public GridBlockListReference GridBlockList;
+	public MapSO Map;
+	public WorldObjectList WorldObjectList;
+
+	private StateClass State => _state ?? (_state = new StateClass());
+
+	public WorldObjectPath GetPathToClosest(GridPosition pos)
+	{
+		switch(State.StateIsFromPosition(pos, Map.Value))
+		{
+			case StateClass.StateResult.Not:
+				NotState(pos);
+				break;
+			case StateClass.StateResult.Same:
+				SameState(pos);
+				break;
+			case StateClass.StateResult.Nearby:
+				NearbyState(pos);
+				break;
+			default:
+				throw new ArgumentOutOfRangeException();
+		}
+		return ShortestPath();
+	}
+
+	private WorldObjectPath ShortestPath() => State.ShortestPath();
+
+	public StateClass GetState(GridPosition pos)
+	{
+		switch(State.StateIsFromPosition(pos, Map.Value))
+		{
+			case StateClass.StateResult.Not:
+				NotState(pos);
+				break;
+			case StateClass.StateResult.Same:
+				SameState(pos);
+				break;
+			case StateClass.StateResult.Nearby:
+				NearbyState(pos);
+				break;
+			default:
+				throw new ArgumentOutOfRangeException();
+		}
+		return State;
+	}
+
+	private void NotState(GridPosition pos)
+	{
+		State.Position = pos;
+		State.Dictionary = new Dictionary<WorldObject, TilePath>(WorldObjectList.Count);
+
+		foreach(var item in WorldObjectList.Items)
+			State.Dictionary.Add(item, Pathfinding.FindPath(pos, item.GPosition, Map.Value).Path);
+	}
+
+	private void SameState(GridPosition pos)
+	{ }
+
+	private void NearbyState(GridPosition pos)
+	{
+		foreach(var path in State.Dictionary)
+		{
+			if(path.Value.Contains(pos))
+			{
+				var index = path.Value.IndexOf(pos);
+				if(index == 0)
+					continue;
+				path.Value.RemoveRange(0, index);
+			}
+			else
+			{
+				if(Map.Value.Neighbours(pos).ContainsPos(path.Value[0]))
+					path.Value.Insert(0, pos);
+				else
+					State.Dictionary[path.Key] = Pathfinding.FindPath(pos, path.Key.GPosition, Map.Value).Path;
+			}
+		}
+	}
+
+	public class WorldObjectPath
+	{
+		public TilePath Path;
+		public WorldObject WorldObject;
+	}
+
+	public class StateClass
+	{
+		public enum StateResult
+		{
+			Not,
+			Same,
+			Nearby
+		}
+
+		public Dictionary<WorldObject, TilePath> Dictionary;
+		public GridPosition Position = GridPosition.MinInt;
+
+		public StateResult StateIsFromPosition(GridPosition pos, Map map)
+		{
+			if((pos == Position) && (Dictionary != null))
+				return StateResult.Same;
+			if(map.Neighbours(pos).ContainsPos(pos) && (Dictionary != null))
+				return StateResult.Nearby;
+			return StateResult.Not;
+		}
+
+		public WorldObjectPath ShortestPath()
+		{
+			var shortPathObj = Dictionary.First().Key;
+			var shortCount = Dictionary[shortPathObj].Count;
+			foreach(var path in Dictionary)
+			{
+				if(path.Key == shortPathObj)
+					continue;
+				if(path.Value.Count < shortCount)
+				{
+					shortPathObj = path.Key;
+					shortCount = path.Value.Count;
+				}
+			}
+			return new WorldObjectPath
+				   {
+					   Path = Dictionary[shortPathObj],
+					   WorldObject = shortPathObj
+				   };
+		}
+	}
+}
--- Assets/Scripts/Jordan/ML/Drone_Agent.cs
+++ Assets/Scripts/Jordan/ML/Drone_Agent.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using ForestOfChaosLib.Attributes;
 using ForestOfChaosLib.Grid;
 using UnityEngine;
@@ -13,9 +14,11 @@ public class Drone_Agent: Agent
 	public MapSO Map;
 	public SpawnRef Spawn;
 	public WorldObjectList WorldObjectList;
+	public WorldObjectPathManager WorldObjectPath;
 	private Map MapVal => Map.Value;
 	public Map.Neighbour GetNeighbours() => MapVal.Neighbours(GPosition, false);
 	private readonly Dictionary<GridPosition, int> walkedOnTiles = new Dictionary<GridPosition, int>();
+	private List<WorldObject> walkedOnWorldObjects = new List<WorldObject>();
 	private float TotalUnfoundRewardLocations;
 
 	[Header("State Values")]
@@ -24,12 +27,18 @@ public class Drone_Agent: Agent
 	[DisableEditing] public float Up;
 	[DisableEditing] public float Down;
 	[DisableEditing] public float UnWalkedTiles;
-	[DisableEditing] public float Distance;
 	[DisableEditing] public float UnfoundLocations;
 	[DisableEditing] public float ActorX;
 	[DisableEditing] public float ActorY;
 	[DisableEditing] public float ExitX;
 	[DisableEditing] public float ExitY;
+	[DisableEditing] public float ExitDistance;
+	[DisableEditing] public float ClosestWODistance;
+
+	[Header("Reward")]
+	[DisableEditing] public float MyReward;
+
+
 
 	private void SetWorldPosition(GridPosition pos)
 	{
@@ -53,16 +62,20 @@ public class Drone_Agent: Agent
 			case -1:
 				return;
 			case 0: //Up|North
-				canMove = neighbours.Neighbours[moveToPosition = GPosition.Up] == TileType.Floor;
+				if(neighbours.ContainsPos(GPosition.Up))
+					canMove = neighbours.Neighbours[moveToPosition = GPosition.Up] == TileType.Floor;
 				break;
 			case 1: //Down|South
-				canMove = neighbours.Neighbours[moveToPosition = GPosition.Down] == TileType.Floor;
+				if(neighbours.ContainsPos(GPosition.Down))
+					canMove = neighbours.Neighbours[moveToPosition = GPosition.Down] == TileType.Floor;
 				break;
 			case 2: //Left|West
-				canMove = neighbours.Neighbours[moveToPosition = GPosition.Left] == TileType.Floor;
+				if(neighbours.ContainsPos(GPosition.Left))
+					canMove = neighbours.Neighbours[moveToPosition = GPosition.Left] == TileType.Floor;
 				break;
 			case 3: //Right|East
-				canMove = neighbours.Neighbours[moveToPosition = GPosition.Right] == TileType.Floor;
+				if(neighbours.ContainsPos(GPosition.Right))
+					canMove = neighbours.Neighbours[moveToPosition = GPosition.Right] == TileType.Floor;
 				break;
 		}
 
@@ -72,7 +85,7 @@ public class Drone_Agent: Agent
 			switch(GetTileTileKind(moveToPosition))
 			{
 				case TileKind.UnWalked:
-					reward = 0.1f;
+					reward = 0.02f;
 					break;
 				case TileKind.Walked:
 					break;
@@ -89,10 +102,12 @@ public class Drone_Agent: Agent
 					throw new ArgumentOutOfRangeException();
 			}
 		}
-		else
+		else if(!canMove)
 		{
 			reward = -0.1f;
 		}
+
+		MyReward += reward;
 	}
 
 	enum TileKind
@@ -121,19 +136,18 @@ public class Drone_Agent: Agent
 
 		if(GridBlockListReference.GetBlock(moveToPosition)?.HasWorldObject == true)
 		{
+			walkedOnWorldObjects.Add(GridBlockListReference.GetBlock(moveToPosition).WorldObject);
 			TotalUnfoundRewardLocations -= 1;
 			return TileKind.Rare;
 		}
 		return TileKind.UnWalked;
 	}
 
-	private float GetNegativeReward() => -0.1f;
-
 	//11 states
 	public override List<float> CollectState()
 	{
 		var neighbours = GetNeighbours();
-		var state = new List<float>(11);
+		var state = new List<float>(12);
 
 		//Directions
 		state.Add(Up = GetStateDataForDirections(neighbours, GPosition.Up));
@@ -146,7 +160,7 @@ public class Drone_Agent: Agent
 		state.Add(UnWalkedTiles);
 
 		//Total places left to go
-		UnfoundLocations = TotalUnfoundRewardLocations / (WorldObjectList.Count - 2);
+		UnfoundLocations = TotalUnfoundRewardLocations / (WorldObjectList.Count - 1);
 		state.Add(UnfoundLocations);
 
 		//Actor Position
@@ -174,8 +188,13 @@ public class Drone_Agent: Agent
 		}
 
 		//Distance to the end tile
-		Distance = GetDistanceToEnd()/ MapVal.Width;
-		state.Add(Distance);
+		ExitDistance = GetDistanceToEnd() / MapVal.Width;
+		state.Add(ExitDistance);
+
+
+		//Distance to the closest WorldObject
+		ClosestWODistance = GetDistanceToClosestWorldObj() / MapVal.Width;
+		state.Add(ClosestWODistance);
 
 		return state;
 	}
@@ -185,9 +204,29 @@ public class Drone_Agent: Agent
 		if(!Exit.Reference)
 			return 100;
 
-		//oldDistance = newDistance;
-		//return newDistance = Pathfinding.FindPath(GPosition, Exit.Reference.GPosition, Map.Value).Path.Count;
 		return Pathfinding.FindPath(GPosition, Exit.Reference.GPosition, Map.Value).Path.Count;
+	}
+
+	private float GetDistanceToClosestWorldObj()
+	{
+		if(!Exit.Reference)
+			return 100;
+
+		var state = WorldObjectPath.GetState(GPosition);
+
+		WorldObject shortPathObj = Exit.Reference;
+		var shortCount = state.Dictionary[Exit.Reference].Count;
+		foreach(var path in state.Dictionary)
+		{
+			if((path.Key == shortPathObj) || walkedOnWorldObjects.Contains(path.Key))
+				continue;
+			if(path.Value.Count < shortCount)
+			{
+				shortPathObj = path.Key;
+				shortCount = path.Value.Count;
+			}
+		}
+		return shortCount;
 	}
 
 	private float GetStateDataForDirections(Map.Neighbour neighbours, GridPosition pos)
@@ -207,6 +246,8 @@ public class Drone_Agent: Agent
 
 	public override void AgentReset()
 	{
+		MyReward = 0;
+		walkedOnWorldObjects.Clear();
 		walkedOnTiles.Clear();
 		Init();
 	}
@@ -217,6 +258,7 @@ public class Drone_Agent: Agent
 	public override void InitializeAgent()
 	{
 		//Init();
+		MyReward = 0;
 		Spawn.OnValueChange -= OnValueChange;
 		Spawn.OnValueChange += OnValueChange;
 	}
@@ -231,5 +273,6 @@ public class Drone_Agent: Agent
 	{
 		TotalUnfoundRewardLocations = WorldObjectList.Count;
 		SetWorldPosition(Spawn.Reference.GPosition);
+		walkedOnWorldObjects.Add(Spawn.Reference);
 	}
 }
