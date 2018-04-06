1b983fdac15e5926e3b9787577d9e50ca54c705c
JMiles42 <J@jmiles42.com>
2018-04-06 13:03:08 +1000

    Removed usless workaround

--- Assets/Scripts/Jordan/WorldObjects/ExitWO.cs
+++ Assets/Scripts/Jordan/WorldObjects/ExitWO.cs
@@ -1,13 +1,2 @@
-﻿using ForestOfChaosLib.AdvVar;
-
-public class ExitWO: WorldObject
-{
-	public ExitRef Reference;
-	public BoolReference HasReference;
-
-	private void OnDestroy()
-	{
-		Reference.Reference = null;
-		HasReference.Value = false;
-	}
-}
+﻿public class ExitWO: WorldObject
+{ }
